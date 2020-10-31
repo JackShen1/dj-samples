c15f1aa94984d4fd1fdbf07a6385ba6315b6b804
Jack Shendrikov <jackshendrikov@gmail.com>
2020-10-31 13:26:03 +0200

    test

--- chat/static/js/app.js
+++ chat/static/js/app.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
     updateUserList();
     disableInput();
 
-    let socket = new WebSocket('wss://' + window.location.host)
+    let socket = new WebSocket('wss://' + window.location.host + '?session_key=${sessionKey}')
 
     chatInput.keypress(function (e) {
         if (e.keyCode === 13)
