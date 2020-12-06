e1c3ffc9181ea59baaa9d17e2d2d0441f9f6a24f
Jack Shendrikov <jackshendrikov@gmail.com>
2020-12-06 21:59:56 +0200

    Delete static

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from django.conf.urls import url
 from django.views.static import serve
 from django.contrib.auth import views as auth_views
-from mysite import settings
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,7 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
     # Sample applications
     path('ads/', include('ads.urls')),
@@ -50,8 +48,7 @@ urlpatterns += [
 
 # Switch to social login if it is configured - Keep for later
 try:
-    from . import github_settings, settings
-
+    from . import github_settings
     social_login = 'registration/login_social.html'
     urlpatterns.insert(0, path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login)))
     print('Using', social_login, 'as the login template')
