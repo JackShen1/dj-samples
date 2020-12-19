d62c3c22a46cdb5e8f4afbc6eb8b08c809cf7fc4
Jack Shendrikov <jackshendrikov@gmail.com>
2020-12-19 21:00:02 +0200

    rickroll

--- xmas-tree/urls.py
+++ xmas-tree/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='xmas-tree/intro.html')),
     path('home/', TemplateView.as_view(template_name='xmas-tree/index.html'), name='xmas-home'),
     path('home/xmas-scene', TemplateView.as_view(template_name='xmas-tree/xmas-scene/index.html'), name='xmas-scene'),
+    path('home/xmas-special', TemplateView.as_view(template_name='xmas-tree/xmas-special/index.html'), name='xmas-special'),
     path('home/xmas-firework', TemplateView.as_view(template_name='xmas-tree/xmas-firework/index.html'), name='xmas-firework'),
     path('home/xmas-game', TemplateView.as_view(template_name='xmas-tree/xmas-game/index.html'), name='xmas-game'),
 
